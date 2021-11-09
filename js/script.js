dbb9ee6578b92f7029e23b2b165d1665353deea7
Frameck <fra9879@gmail.com>
2021-11-09 15:45:52 +0100

    Completed exercise with bonus and added time calculation

--- js/script.js
+++ js/script.js
@@ -2,5 +2,28 @@ Vue.config.devtools = true
 
 const vueApp = new Vue({
     el: '#app',
-    data: {}
+    data: {
+        emails: [],
+        timeElapsed: undefined
+    },
+    methods: {
+        getEmails() {
+            const url = 'https://flynn.boolean.careers/exercises/api/random/mail'
+            
+            let initialTime = performance.now()
+
+            // ripeto la API call per 10 volte
+            for (let i = 0; i < 10; i++) {
+                axios.get(url).then(res => {
+                    const email = res.data.response
+                    this.emails.push(email)
+                })
+            }
+
+            let finalTime = performance.now()
+
+            // calcolo quanto tempo ha impiegato il server per darmi tutte le risposte
+            this.timeElapsed = (finalTime - initialTime).toFixed(2)
+        }
+    }
 })
